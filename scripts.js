407d0535aeea362b23b5b74ea879e1a1a0f6f235
conor <doyle.conorp@gmail.com>
2022-04-22 12:39:12 -0700

    Build outline of script page, create shell functs

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// Important variables
+const grid = document.getElementById('grid');
+let cell = document.createElement('div');
+    cell.innerHTML = 'Test';
+
+// Make default grid
+function makeGrid() {
+    makeRows(16);
+    makeColumns(16);
+}
+
+// Helper functions
+function makeRows(num){
+
+}
+
+function makeColumns(num) {
+
+}
+

242120dec32bf8d2b883b51e58800cc0a2ab2716
conor <doyle.conorp@gmail.com>
2022-05-06 10:25:11 -0700

    Add comments to makeTable function

--- scripts.js
+++ scripts.js
@@ -10,12 +10,13 @@ function makeGrid() {
 // Helper functions
 
 function makeTable(num){
+    // Create Rows
     for (let i=0; i<num; i++) {
         let row = document.createElement('div');
         row.className = `x${i+1}` // Give X coordinate
         grid.appendChild(row);
         
-        //add
+        // Create columns per row
         for (let j=0; j<num; j++) {
             let cell = document.createElement('div');
             row.appendChild(cell);
